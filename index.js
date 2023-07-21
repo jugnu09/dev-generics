f3bbc95b3b1450cde6d4373f35e6477608accac6
Ritesh <Bisen@192.168.1.7>
2023-07-21 11:22:17 +0530

    added more utils

--- index.js
+++ index.js
@@ -2,4 +2,23 @@
 export function isArrayHasDuplicateItem(array) {
     const uniqueSet = new Set(array);
     const isArrayUnique = uniqueSet.size!== array.length;
+    return isArrayUnique;
+}
+
+export function getUniqueArray(array) {
+    let uniqueArray =[];
+    array.forEach((item) => {
+      if(!(uniqueArray.indexOf(item) > -1)){
+        uniqueArray.push(item);
+      }
+    })
+    return uniqueArray;
+}
+
+export function getArraySum(array) {
+    let sum = 0;
+    array.forEach(item => {
+        sum = sum+item;
+    })
+    return sum;
 }
